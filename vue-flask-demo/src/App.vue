3b93ecb660e81b931c13542c1e36e500f3518596
Arthuring <20373091@buaa.edu.cn>
2022-08-07 20:21:00 +0800

    init

---
+++ vue-flask-demo/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+<!--    <router-link to="/">Home</router-link> |-->
+<!--    <router-link to="/about">About</router-link>-->
+<!--    头部-->
+    <Headerup/>
+<!--    主体-->
+    <div style="display: flex">
+<!--      侧边栏-->
+      <Aside />
+<!--      内容-->
+      <router-view style="flex: 1"/>
+    </div>
+  </div>
+</template>
+
+<!--<style>-->
+<!--#app {-->
+<!--  font-family: Avenir, Helvetica, Arial, sans-serif;-->
+<!--  -webkit-font-smoothing: antialiased;-->
+<!--  -moz-osx-font-smoothing: grayscale;-->
+<!--  text-align: center;-->
+<!--  color: #2c3e50;-->
+<!--}-->
+
+<!--nav {-->
+<!--  padding: 30px;-->
+<!--}-->
+
+<!--nav a {-->
+<!--  font-weight: bold;-->
+<!--  color: #2c3e50;-->
+<!--}-->
+
+<!--nav a.router-link-exact-active {-->
+<!--  color: #42b983;-->
+<!--}-->
+<!--</style>-->
+
+<script>
+import Headerup from "@/components/Headerup";
+import Aside from "@/components/Aside";
+
+export default {
+  name: "Layout",
+  components:{
+    Headerup,
+    Aside
+  }
+}
+</script>
+
---
+++ vue-flask-demo/src/components/Headerup.vue
@@ -0,0 +1,34 @@
+<template>
+  <div style="height: 50px; line-height: 50px; border-bottom: 1px solid #ccc; display: flex">
+    <div style="width: 200px; padding-left: 30px; font-weight: bold; color: dodgerblue">Question Bank</div>
+    <div style="flex: 1"></div>
+    <div style="width: 150px">
+      <el-avatar shape="circle" :size="30" :src="circleUrl" style="margin-right: 5px; margin-top: 7px"  />
+      <el-dropdown>
+    <span class="el-dropdown-link">
+      <br>
+      User name
+      <el-icon class="el-icon--right">
+        <arrow-down/>
+      </el-icon>
+    </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>Information</el-dropdown-item>
+            <el-dropdown-item>Settings</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Headerup"
+}
+</script>
+
+<style scoped>
+
+</style>
