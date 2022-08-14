5656fe9ca8a70c9fc5c699abc9ddfa33c2871208
Arthuring <20373091@buaa.edu.cn>
2022-08-15 02:52:55 +0800

    fixed style

--- vue-flask-demo/src/views/Analysis/HistoryScore.vue
+++ vue-flask-demo/src/views/Analysis/HistoryScore.vue
@@ -1,18 +1,38 @@
 <template>
-  <el-table :data="tableData" max-height="500"  stripe :table-layout="'auto'" style="margin-top: 5px">
-    <template #empty>
-      <div class="flex items-center justify-center h-100%">
-        <el-empty/>
-      </div>
-    </template>
-    <el-table-column type="index" width="50"/>
-    <el-table-column sortable prop="time" label="Time"/>
-    <el-table-column sortable prop="wrong" label="Number of wrong answer"/>
-    <el-table-column prop="wrongID" label="Wrong ID"/>
-    <el-table-column sortable prop="total" label="Total"/>
-    <el-table-column sortable prop="accuracy" label="Accuracy"/>
-  </el-table>
-
+  <el-container style="flex-direction: column; padding: 10px 0">
+    <el-header height="20px">
+      <el-row type="flex">
+        <el-col :span="8" style="color: #409eff;font-weight: bolder; font-size: large">
+          History Score
+        </el-col>
+        <el-col :span="4"/>
+        <el-col :span="12">
+          <search-bar/>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-main style="overflow: auto">
+      <el-table :data="tableData" max-height="500"  stripe :table-layout="'auto'">
+        <template #empty>
+          <div class="flex items-center justify-center h-100%">
+            <el-empty/>
+          </div>
+        </template>
+        <el-table-column type="index" width="50"/>
+        <el-table-column sortable prop="time" label="Time"/>
+        <el-table-column sortable prop="wrong" label="Number of wrong answer"/>
+        <el-table-column prop="wrongID" label="Wrong ID"/>
+        <el-table-column sortable prop="total" label="Total"/>
+        <el-table-column sortable prop="accuracy" label="Accuracy"/>
+      </el-table>
+    </el-main>
+    <el-footer style="position: absolute; bottom: 0">
+      <el-pagination :currentPage="currentPage" :page-size="pageSize" :page-sizes="[5, 10, 15]" :small="small"
+                     layout="total, sizes, prev, pager, next, jumper" :total="totalPage"
+                     @size-change="handleSizeChange"
+                     @current-change="handleCurrentChange"/>
+    </el-footer>
+  </el-container>
 </template>
 
 <script>
