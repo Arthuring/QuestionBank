9a8abc17bb030595fe152b298c45f13eb25383fc
StyWang <bjwangjunshi@sina.com>
2022-08-14 22:15:06 +0800

    增加 Analysis 中的导航栏

--- vue-flask-demo/src/views/Analysis.vue
+++ vue-flask-demo/src/views/Analysis.vue
@@ -1,5 +1,26 @@
 <template>
-Analysis
+  <div style="">
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+      @select="handleSelect"
+    > <!-- TODO: 更改颜色样式 -->
+      <el-menu-item index="1">Wrong Questions List</el-menu-item>
+      <el-menu-item index="2">Star Questions List</el-menu-item>
+      <el-menu-item index="3">Score Analysis</el-menu-item>
+    </el-menu>
+  </div>
+  <div style="float: left;">
+  ?
+  </div>
+
+  <div style="float: left;">
+    Hi
+  </div>
 </template>
 
 <script>
@@ -7,7 +28,3 @@ export default {
   name: "Analysis"
 }
 </script>
-
-<style scoped>
-
-</style>
